f97646ebc98323d6e543e5870386e165ef7f0d2b
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-07-12 23:07:22 -0500

    Promesas

---
+++ src/promise/index.js
@@ -0,0 +1,19 @@
+const promise = new Promise(function(resolve, reject){
+    resolve('Hey!')
+});
+
+const vacas = 5;
+
+const countVacas = new Promise(function(resolve, reject){
+    if(vacas > 10) {
+        resolve(`Si tenemos ${vacas} vacas suficientes`);
+    } else {
+        reject('No hay vacas Suficientes');
+    }
+});
+
+countVacas.then((result) =>{
+    console.log(result);
+}).catch((error) =>{
+    console.log(error);
+}).finally(()=>console.log('finally'))
